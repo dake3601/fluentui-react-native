806a6cef5126a9909f68aa9b736b8972141997ae
sriya112 <sriyaaluru@gmail.com>
2021-09-02 15:37:36 -0700

    removed extra tokens on macos

--- packages/experimental/Tabs/src/TabsItemTokens.macos.ts
+++ packages/experimental/Tabs/src/TabsItemTokens.macos.ts
@@ -7,7 +7,6 @@ export const tabsItemStates: (keyof TabsItemTokens)[] = ['hovered', 'selected', 
 
 export const defaultTabsItemTokens: TokenSettings<TabsItemTokens, Theme> = (t: Theme) =>
   ({
-    backgroundColor: 'transparent',
     color: t.colors.bodyText,
     borderColor: 'transparent',
     iconColor: t.colors.buttonIcon,
@@ -21,32 +20,18 @@ export const defaultTabsItemTokens: TokenSettings<TabsItemTokens, Theme> = (t: T
 
     disabled: {
       color: t.colors.buttonTextDisabled,
-      borderColor: 'transparent',
-      iconColor: t.colors.buttonDisabledIcon,
       indicatorColor: t.colors.transparent,
       fontWeight: 'normal',
     },
     hovered: {
-      backgroundColor: t.colors.neutralForeground2Hover,
-      iconColor: t.colors.buttonHoveredIcon,
-      color: 'buttonTextHovered',
       fontWeight: 'bold',
-      indicatorColor: t.colors.neutralStroke1,
       selected: {
-        marginHorizontal: -1,
+        marginHorizontal: 0,
       },
     },
-    focused: {
-      color: t.colors.neutralForeground1,
-      icon: t.colors.buttonFocusedIcon,
-      borderWidth: 2,
-      borderRadius: 4,
-    },
     selected: {
-      color: t.colors.buttonTextHovered,
       icon: t.colors.buttonFocusedIcon,
       indicatorColor: t.colors.accentButtonBackground,
-      variant: 'bodySemibold',
-      iconColor: t.colors.buttonDisabledIcon,
+      fontWeight: 'bold',
     },
   } as TabsItemTokens);
